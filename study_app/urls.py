493fc0ecf97a75aedc67c7506dda792724a29f15
Yernar777 <yernar20082002@gmail.com>
2024-12-18 22:06:13 +0500

    gotovo

--- study_app/urls.py
+++ study_app/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('forms/',views.review_functional,name='reviews_form'),
     path('succes/',views.secces,name='succes'),
     path('auth/',views.login_view,name='auth'),
+    path('cart/', views.cart_detail, name='cart_detail')
 
 ]
